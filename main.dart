dd89a401523986dcc3cfdcb67cc47452b8c8ffec
Kavishka Rajapaskhe <61306473+KavishkaRajapakshe@users.noreply.github.com>
2021-02-08 19:22:10 +0530

    Add files via upload
    The dart files for the Login and Sign up page

---
+++ main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'signup.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String,WidgetBuilder>{
+        '/signup':(BuildContext context)=> new SignupPage()
+      },
+      home:new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  @override
+  _MyHomePageState createState()=> new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build (BuildContext context){
+    return new Scaffold(
+      backgroundColor:Colors.indigo[900],
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+            child: Text('OCELLUS',
+              style: TextStyle(
+                fontSize: 80.0,fontWeight: FontWeight.bold,color: Colors.deepOrange),
+            ),
+          ),
+          SizedBox(height: 50.0,),
+          Container(padding: EdgeInsets.only(top: 35.0,left: 20.0,right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'USER NAME',
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.orange)
+                    )
+                  ),
+                ),
+                SizedBox(height: 50.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'PASSWORD',
+                      labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.orange)
+                      )
+                  ),
+                  obscureText: true,
+                ),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top: 15.0,left:20.0),
+                  child: InkWell(
+                    child:Text('Forgot Password',
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.deepOrangeAccent,
+                    color: Colors.deepOrange,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){},
+                      child: Center(
+                        child: Text('SIGN IN',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'New to Ocellus ?',
+                style: TextStyle(color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(width: 5.0),
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).pushNamed('/signup');
+                },
+                child: Text('Sign up',
+                  style:TextStyle(color: Colors.orange,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
