8e2a56c6e384226e220d41fce29442bf89710dd3
himayaperera <60315027+himayaperera@users.noreply.github.com>
2021-03-29 20:04:51 +0530

    Add files via upload

--- routes/image.js
+++ routes/image.js
@@ -3,17 +3,17 @@ const express = require('express'); //import express
 // 1.
 const router  = express.Router(); 
 // 2.
-const teaController = require('../controllers/image'); 
+const ImageController = require('../controllers/image'); 
 // 3.
 //router.post('/image', imageController.newImage); 
 //router.post('/image2',imageController.newImage2);
-router.post('/tea', teaController.uploadImg /*insert this guy*/ , teaController.newTea);
-router.get('/tea', teaController.getAllTea);
-router.delete('/tea', teaController.deleteAllTea);
+router.post('/Image', ImageController.uploadImg /*insert this guy*/ , ImageController.newImage);
+router.get('/Image', ImageController.getAllImage);
+router.delete('/Image', ImageController.deleImagellImage);
 
-router.get('/tea/:name', teaController.getOneTea);
-router.post('/tea/:name', teaController.newComment);
-router.delete('/tea/:name', teaController.deleteOneTea);
+router.get('/Image/:name', ImageController.getOneImage);
+router.post('/Image/:name', ImageController.newComment);
+router.delete('/Image/:name', ImageController.deleteOneImage);
 
 // 4. 
 module.exports = router; // export to use in server.js
